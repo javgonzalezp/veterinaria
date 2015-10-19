ce0f7fc5b36f44838ee416eee450d2807c188a76
javgonzalezp <javgonzalezp@gmail.com>
2015-10-18 22:50:03 -0300

    limpieza

--- src/main/java/com/clinica/veterinaria/base/dao/RowMapper.java
+++
@@ -1,8 +0,0 @@
-package com.clinica.veterinaria.base.dao;
-
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
-public interface RowMapper<T> {
-	public abstract T mapRow(ResultSet paramResultSet) throws SQLException;	
-}
